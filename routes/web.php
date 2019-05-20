7ac4c1692e63f5115c0f568372b0d5a8fefa8959
vagrant <vagrant@homestead>
2019-05-20 10:18:59 +0000

    TP-C: routes

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,23 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Ceci est la page d’accueil";
+});
+
+Route::get('/voyages/{id_voyage?}', function ($id_voyage = null) {
+    return "Ceci est la page qui affichera le detaild’un voyage identifié par $id_voyage";
+})->where('id_voyage',	'[0-9]+');
+
+Route::get('/a_propos', function () {
+    return "Ceci est la page qui affichera les informations concernant l’entreprise";
+});
+
+Route::prefix('admin')->group(function () {	
+    Route::get('/voyages', function () {
+        return "Ceci est la page voyages de la console d’administration";
+    });
+
+    Route::get('/users', function () {
+        return "Ceci est la page users de la console d’administration";
+    });
 });
