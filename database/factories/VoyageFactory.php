ff8f685ce63cc5df9c1c27e3a8c880c180c61896
vagrant <vagrant@homestead>
2019-05-21 10:20:10 +0000

    TP-G

---
+++ database/factories/VoyageFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Voyage;
+use Faker\Generator as Faker;
+
+$factory->define(Voyage::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'image' => $faker->image,
+        'price' => $faker->randomFloat(2, 50, 3000),
+        'destination' => $faker->country,
+        'description' => $faker->text
+    ];
+});
